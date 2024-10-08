a42ec4d171aefc899775efbd7ceb4d810b4eda9d
dalhatmd <umarwalid4055@gmail.com>
2024-10-08 18:21:06 +0000

    Fix pycodestyle,

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,20 +3,18 @@
 
 
 def canUnlockAll(boxes):
-	""" Returns true if all boxes can be unlocked, false otherwise"""
-	
-	n = len(boxes)
-	unlocked = [False] * n
-	unlocked[0] = True
+    """ Returns true if all boxes can be unlocked, false otherwise"""
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    keys_to_try = boxes[0]
 
-	keys_to_try = boxes[0]
-	
-	while keys_to_try:
-		new_keys_to_try = []
-		for key in keys_to_try:
-			if 0 <= key < n and not unlocked[key]:
-				unlocked[key] = True
-				new_keys_to_try.extend(boxes[key])
-		keys_to_try = new_keys_to_try
+    while keys_to_try:
+        new_keys_to_try = []
+        for key in keys_to_try:
+            if 0 <= key < n and not unlocked[key]:
+                unlocked[key] = True
+                new_keys_to_try.extend(boxes[key])
+        keys_to_try = new_keys_to_try
 
-	return (all(unlocked))
+    return (all(unlocked))
