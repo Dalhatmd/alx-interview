fe9bbaf90a5d0576640191161264a97576e98330
dalhatmd <umarwalid4055@gmail.com>
2024-10-08 18:08:36 +0000

    Add lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" solution module """
+
+
+def canUnlockAll(boxes):
+	""" Returns true if all boxes can be unlocked, false otherwise"""
+	
+	n = len(boxes)
+	unlocked = [False] * n
+	unlocked[0] = True
+
+	keys_to_try = boxes[0]
+	
+	while keys_to_try:
+		new_keys_to_try = []
+		for key in keys_to_try:
+			if 0 <= key < n and not unlocked[key]:
+				unlocked[key] = True
+				new_keys_to_try.extend(boxes[key])
+		keys_to_try = new_keys_to_try
+
+	return (all(unlocked))
