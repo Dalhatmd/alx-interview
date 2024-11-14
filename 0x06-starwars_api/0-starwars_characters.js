94297ca9fd9d2cad0c16904433c7f9cad1aa4911
dalhatmd <umarwalid4055@gmail.com>
2024-11-14 15:41:38 +0100

    Use promise for async processing

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,32 +5,35 @@ const request = require('request');
 const film = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${film}`;
 
-function getCharacters () {
-  request(url, (error, response, body) => {
-    if (error) {
-      console.log('Error:', error);
-      return;
-    }
-
-    if (response.statusCode === 200) {
-      const data = JSON.parse(body);
-      const characterLinks = data.characters;
-
-      characterLinks.forEach((characterUrl) => {
-        request(characterUrl, (error, response, body) => {
-          if (error) {
-            console.log('Error:', error);
-            return;
-          }
-
-          if (response.statusCode === 200) {
-            const character = JSON.parse(body);
-            console.log(character.name);
-          }
-        });
-      });
-    }
+function fetchUrl (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) return reject(error);
+      if (response.statusCode === 200) {
+        resolve(JSON.parse(body));
+      } else {
+        reject(new Error(`Request failed with status ${response.statusCode}`));
+      }
+    });
   });
+}
+
+async function getCharacters () {
+  try {
+    // Fetch the film data
+    const filmData = await fetchUrl(url);
+    const characterLinks = filmData.characters;
+
+    const characterPromises = characterLinks.map(fetchUrl);
+
+    const characters = await Promise.all(characterPromises);
+
+    characters.forEach((character) => {
+      console.log(character.name);
+    });
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
 
 getCharacters();
