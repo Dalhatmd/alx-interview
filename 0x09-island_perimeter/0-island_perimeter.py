c75da6a2bde01a692dc2cc9dd8667cf952db82d1
Dalhatmd <umarwalid4055@gmail.com>
2024-12-02 16:36:39 +0100

    Add task 0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Island perimeter """
+
+
+def island_perimeter(grid):
+    """Calculates the perimeter of an island in the grid."""
+    perimeter = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                perimeter += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1: 
+                    perimeter -= 2
+
+    return perimeter
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,1 @@
+## Directory for island perimeter task
